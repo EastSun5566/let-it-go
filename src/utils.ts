160ae9c3b33c50a80fabfece22311b9160ab002c
汪東陽 <michael19920327@gmail.com>
2020-10-09 00:36:55 +0800

    chore: typing

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ export const getRandomNumber = (
   max: number,
 ): number => Math.random() * (max - min) + min;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const debounce = <F extends (...args: any[]) => any>(
   fn: F,
   ms = 250,
