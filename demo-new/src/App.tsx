8ba37b762cd414b31bc55a2e6d1401a682bf073f
汪東陽 <michael19920327@gmail.com>
2022-12-11 04:28:47 +0800

    feat: add tailwind

--- demo-new/src/App.tsx
+++ demo-new/src/App.tsx
@@ -1,17 +1,15 @@
 import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
-import styles from './App.module.css';
 
 const App: Component = () => (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+    <div>
+      <header>
+        <img src={logo} class="w-[300px]" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          class={styles.link}
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
