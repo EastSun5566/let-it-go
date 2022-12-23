2763b0b3b6ad9aa66aecd3898531150a5b85c6f0
汪東陽 <michael19920327@gmail.com>
2022-12-24 04:13:34 +0800

    feat: add some components

--- demo-new/src/App.tsx
+++ demo-new/src/App.tsx
@@ -1,23 +1,65 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
+import { GithubCorner } from './components';
 
-const App: Component = () => (
-    <div>
-      <header>
-        <img src={logo} class="w-[300px]" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+export const App: Component = () => (
+  <>
+    <header class="text-center">
+      <h3>❄️ Welcome to LET IT GO</h3>
+      <h5>Let your website snow instantly ⛄️</h5>
+
+      <small class="d-block mt-3">
+        Made with ❤️ By
         <a
-          href="https://github.com/solidjs/solid"
+          href="https://github.com/EastSun5566"
+          class="badge badge-pill badge-primary"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn Solid
+          EastSun5566
         </a>
-      </header>
-    </div>
+      </small>
+    </header>
+
+    <main>
+      <div>
+        <div>
+          <input
+            type="checkbox"
+            id="is-snow"
+            checked
+          />
+          <label for="is-snow" />
+        </div>
+
+        <div>
+          <button type="button" id="reset">
+            RESET
+          </button>
+          <button type="button" id="toggle" />
+        </div>
+      </div>
+
+      <div>
+        <label for="number">❄️ NUMBER</label>
+        <input
+          type="number"
+          id="number"
+          placeholder="number of snowflake"
+        />
+      </div>
+
+      <div>
+        <label for="number">❄️ COLOR</label>
+        <input
+          type="color"
+          id="color"
+          placeholder="color of snowflake"
+        />
+      </div>
+    </main>
+
+    <GithubCorner />
+  </>
 );
 
 export default App;
--- demo-new/src/main.tsx
+++ demo-new/src/main.tsx
@@ -2,6 +2,6 @@
 import { render } from 'solid-js/web';
 
 import './assets/index.css';
-import App from './App';
+import { App } from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
