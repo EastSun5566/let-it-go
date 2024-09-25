e869da4844d1925c60726ae25d365bc51089d748
Michael Wang <michael19920327@gmail.com>
2024-09-25 18:26:12 +0800

    fix: fix build & add ci build task

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,9 @@ import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 
-import { name, main, module } from './package.json';
+import pkg from './package.json' assert { type: 'json' };
+
+const { name, main, module } = pkg;
 
 /**
  * @param {string} string
